45f1d0e128c0e5e3d8597431defb86df274f269d
GAURAV-N-PATIL <gauravnpatil242007@gmail.com>
2026-08-11 12:01:46 +0530

    chore: initialize spring boot backend with maven wrapper and gitignore

---
+++ cloudlab-backend/src/main/java/cloudlab_backend/CloudlabBackendApplication.java
@@ -0,0 +1,13 @@
+package cloudlab_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudlabBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudlabBackendApplication.class, args);
+	}
+
+}

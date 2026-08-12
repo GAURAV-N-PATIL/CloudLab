64c5c2d6bc2ad47f2b6248be9ab97081bf97fdb2
GAURAV-N-PATIL <gauravnpatil242007@gmail.com>
2026-08-12 10:37:49 +0530

    feat: add JWT security configuration, filters, and user repository methods

--- cloudlab-backend/src/main/java/cloudlab_backend/repository/UserRepository.java
+++ cloudlab-backend/src/main/java/cloudlab_backend/repository/UserRepository.java
@@ -1,5 +1,8 @@
 package cloudlab_backend.repository;
 import cloudlab_backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-public interface UserRepository extends JpaRepository<User,Long> {
+import java.util.Optional;
+public interface UserRepository extends JpaRepository<User,Long>{
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
 }

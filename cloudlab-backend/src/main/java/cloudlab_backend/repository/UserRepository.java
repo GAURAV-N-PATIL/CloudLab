61a5b67726f7e21138c0e3a12a6e31b05d29867e
GAURAV-N-PATIL <gauravnpatil242007@gmail.com>
2026-09-25 20:57:37 +0530

    refactor: add entity graph to optimize selectedcloud fetching

--- cloudlab-backend/src/main/java/cloudlab_backend/repository/UserRepository.java
+++ cloudlab-backend/src/main/java/cloudlab_backend/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package cloudlab_backend.repository;
 import cloudlab_backend.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 public interface UserRepository extends JpaRepository<User,Long>{
+    @EntityGraph(attributePaths = "selectedCloud")
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
 }
